8eeee391b9113c142ce07e0aa4134433cf26d878
Smith Egharevba <57641192+BlaqSmyth@users.noreply.github.com>
2025-03-01 13:32:11 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,8 +6,9 @@ def testApp() {
     echo 'testing the application...'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def deployApp() {
+    echo 'deploying the application...'
+    echo “deploying version ${params.VERSION}”
 }
 
 return this
